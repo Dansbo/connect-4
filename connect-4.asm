a5200917334a96a046e6d75e68e60ef2a649bb4b
Dansbo <erik@dansbo.dk>
2021-02-27 23:23:18 +0100

    Changed so that I made my own boilerplate instead of using macro

--- connect-4.asm
+++ connect-4.asm
@@ -1,9 +1,15 @@
 !cpu w65c02
-
+;******************************* Boiler plate *********************************
+*=$801
+	!word	$080C			; Pointer to next BASIC line
+	!word	$0001			; Line number $0001 = 1
+	!byte	$9E			; SYS BASIC token
+	!pet	" $810",0		; Address where ASM starts
+	!word	$0000			; EOF BASIC program
+*=$810
 ;********************************Inclusions************************************
 !src	"cx16stuff/cx16.inc"
 !src	"cx16stuff/vera0.9.inc"
 !src	"bin2vera.inc"
-	+SYS_LINE
 	jsr Init_VERA
 	rts

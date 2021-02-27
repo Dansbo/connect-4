713f7c2400d83e7f5f89b4ac06d09167dfaa1213
Dansbo <erik@dansbo.dk>
2021-02-27 23:15:19 +0100

    Moved macro back under the src

--- connect-4.asm
+++ connect-4.asm
@@ -1,9 +1,9 @@
 !cpu w65c02
-	+SYS_LINE
+
 ;********************************Inclusions************************************
 !src	"cx16stuff/cx16.inc"
 !src	"cx16stuff/vera0.9.inc"
 !src	"bin2vera.inc"
-
+	+SYS_LINE
 	jsr Init_VERA
 	rts

9364a3e00db218b015b34b3ddd57c8a50b0d0700
Dansbo <erik@dansbo.dk>
2021-02-27 21:04:03 +0100

    Replaced boilerplate with macro

--- connect-4.asm
+++ connect-4.asm
@@ -1,16 +1,9 @@
 !cpu w65c02
 
-*=$0801
-	!word	$080C			; Pointer to next BASIC line
-	!word	$0001			; Line number $0001 = 1
-	!byte	$9E			; SYS BASIC token
-	!pet	" $810",0		; Address where ASM starts
-	!word	$0000			; EOF BASIC program
-*=$0810
-
 ;********************************Inclusions************************************
 !src	"cx16stuff/cx16.inc"
 !src	"cx16stuff/vera0.9.inc"
 
+	+SYS_LINE
 
 rts
